c7e2bb6c74eed60a21066546a6c18419f625b4d8
edmbernard <bernard.erwan@gmail.com>
2020-12-01 22:51:34 +0100

    feat(day1): change test formatting

--- src/day1-catch2.cpp
+++ src/day1-catch2.cpp
@@ -1,22 +1,21 @@
 #include <tuple>
 
-#include <catch2/catch.hpp>
 #include "day1/day1.hpp"
+#include <catch2/catch.hpp>
 
 TEST_CASE("AoC2020 Day1", "[day1]") {
 
-  auto [filename, expectedPart1, expectedPart2] = GENERATE( table<std::string, size_t, size_t>({
+  auto [filename, expectedPart1, expectedPart2] = GENERATE(table<std::string, size_t, size_t>({
       std::make_tuple("day1/input_part1_test1.txt", 514579, 241861950),
       std::make_tuple("day1/input.txt", 381699, 111605670)
-    } ) );
+    }));
 
   std::vector<size_t> numberList = parseInputFile(filename);
 
-  SECTION("Part1") {
+  SECTION("Part1 :" + filename) {
     CHECK(find2Sum(numberList) == expectedPart1);
   }
-  SECTION("Part2") {
+  SECTION("Part2 :" + filename) {
     CHECK(find3Sum(numberList) == expectedPart2);
   }
-
 }
