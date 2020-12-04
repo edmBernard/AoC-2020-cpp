c27f8f11dc67331a3ac9b60e45699d33bd70cd48
edmbernard <bernard.erwan@gmail.com>
2020-12-04 09:49:53 +0100

    feat(bench): add day4

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -5,6 +5,7 @@
 #include "day1/day1.hpp"
 #include "day2/day2.hpp"
 #include "day3/day3.hpp"
+#include "day4/day4.hpp"
 
 
 int main() {
@@ -12,23 +13,34 @@ int main() {
   double totalCount = 0;
   {
     auto start_temp = std::chrono::high_resolution_clock::now();
-    day1::part1(day1::parseInputFile("input/day1/input.txt"));
+    day1::part1(day1::parseInputFile("day1/input.txt"));
+    day1::part2(day1::parseInputFile("day1/input.txt"));
     std::chrono::duration<double, std::milli> elapsed_temp = std::chrono::high_resolution_clock::now() - start_temp;
     fmt::print("day1 : {} ms \n", elapsed_temp.count());
     totalCount += elapsed_temp.count();
   }
   {
     auto start_temp = std::chrono::high_resolution_clock::now();
-    day2::part1(day2::parseInputFile("input/day2/input.txt"));
+    day2::part1(day2::parseInputFile("day2/input.txt"));
+    day2::part2(day2::parseInputFile("day2/input.txt"));
     std::chrono::duration<double, std::milli> elapsed_temp = std::chrono::high_resolution_clock::now() - start_temp;
     fmt::print("day2 : {} ms \n", elapsed_temp.count());
     totalCount += elapsed_temp.count();
   }
   {
     auto start_temp = std::chrono::high_resolution_clock::now();
-    day3::part1(day3::parseInputFile("input/day3/input.txt"));
+    day3::part1(day3::parseInputFile("day3/input.txt"));
+    day3::part2(day3::parseInputFile("day3/input.txt"));
     std::chrono::duration<double, std::milli> elapsed_temp = std::chrono::high_resolution_clock::now() - start_temp;
     fmt::print("day3 : {} ms \n", elapsed_temp.count());
+    totalCount += elapsed_temp.count();
+  }
+  {
+    auto start_temp = std::chrono::high_resolution_clock::now();
+    day4::part1(day4::parseInputFile("day4/input.txt"));
+    day4::part2(day4::parseInputFile("day4/input.txt"));
+    std::chrono::duration<double, std::milli> elapsed_temp = std::chrono::high_resolution_clock::now() - start_temp;
+    fmt::print("day4 : {} ms \n", elapsed_temp.count());
     totalCount += elapsed_temp.count();
   }
 
