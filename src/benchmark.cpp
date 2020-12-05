093ea30cf52cc897f7f6ce8af1cffdde90b9eab7
edmbernard <bernard.erwan@gmail.com>
2020-12-05 17:54:12 +0100

    feat(bench): update to take allinone solution

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -9,28 +9,45 @@
 #include "day5/day5.hpp"
 
 // Temporary until I find a nicer solution without macro :(
-#define BENCH(x)                                                                                                     \
-  double day##x = 0;                                                                                            \
-  for (int i = 0; i < repetition; ++i) {                                                                             \
-    auto start_temp = std::chrono::high_resolution_clock::now();                                                     \
-    day##x::part1(day##x::parseInputFile("day" + std::to_string(x) + "/input.txt"));                                 \
-    day##x::part2(day##x::parseInputFile("day" + std::to_string(x) + "/input.txt"));                                 \
-    std::chrono::duration<double, std::milli> elapsed_temp = std::chrono::high_resolution_clock::now() - start_temp; \
-    day##x += elapsed_temp.count();                                                                                  \
-  }                                                                                                                  \
-  totalCount += day##x;                                                                                              \
-  fmt::print("day{} : {:.2f} ms \n", x, day##x / repetition);
+#define BENCH_SPLIT(x)                                                                                                 \
+  {                                                                                                                    \
+    double time_day##x = 0;                                                                                            \
+    for (int i = 0; i < repetition; ++i) {                                                                             \
+      auto start_temp = std::chrono::high_resolution_clock::now();                                                     \
+      auto res = day##x::parseInputFile("day" + std::to_string(x) + "/input.txt");                                     \
+      day##x::part1(res);                                                                                              \
+      day##x::part2(res);                                                                                              \
+      std::chrono::duration<double, std::milli> elapsed_temp = std::chrono::high_resolution_clock::now() - start_temp; \
+      time_day##x += elapsed_temp.count();                                                                             \
+    }                                                                                                                  \
+    totalCount += time_day##x;                                                                                         \
+    fmt::print("Day{} Splitted: {:.2f} ms \n", x, time_day##x / repetition);                                           \
+  }
+
+#define BENCH(x)                                                                                                       \
+  {                                                                                                                    \
+    double time_day##x = 0;                                                                                            \
+    for (int i = 0; i < repetition; ++i) {                                                                             \
+      auto start_temp = std::chrono::high_resolution_clock::now();                                                     \
+      auto [part1, part2] = day##x::allInOne("day" + std::to_string(x) + "/input.txt");                                \
+      std::chrono::duration<double, std::milli> elapsed_temp = std::chrono::high_resolution_clock::now() - start_temp; \
+      time_day##x += elapsed_temp.count();                                                                             \
+    }                                                                                                                  \
+    totalCount += time_day##x;                                                                                         \
+    fmt::print("Day{} AllInOne: {:.2f} ms \n", x, time_day##x / repetition);                                           \
+  }
 
 int main() {
 
   double totalCount = 0;
   const int repetition = 100;
 
-  BENCH(1);
-  BENCH(2);
-  BENCH(3);
+  BENCH_SPLIT(1);
+  BENCH_SPLIT(2);
+  BENCH_SPLIT(3);
+  BENCH_SPLIT(4);
   BENCH(4);
-  BENCH(5);
+  BENCH_SPLIT(5);
 
   fmt::print("Total duration per run : {:.2f} ms \n", totalCount / repetition);
 
