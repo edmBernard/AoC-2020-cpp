5ec618a86026b210d2c6eb7dedf854ff0c59c073
edmbernard <bernard.erwan@gmail.com>
2021-05-22 14:54:09 +0200

    feat(day19): add solution part1

---
+++ test/day19-catch2.cpp
@@ -0,0 +1,24 @@
+#include <tuple>
+#include <iostream>
+
+#include <catch2/catch.hpp>
+
+#include "day19/day19.hpp"
+
+TEST_CASE("AoC2020 Day19", "[day19]") {
+
+  auto [filename, expectedPart1, expectedPart2] = GENERATE(table<std::string, size_t, size_t>({
+      std::make_tuple("day19/input_part1_test1.txt", 1, 0),
+      std::make_tuple("day19/input_part1_test2.txt", 2, 0),
+      std::make_tuple("day19/input.txt", 144, 0)
+    }));
+
+  auto res = day19::parseInputFile(filename);
+
+  SECTION("Part1 : " + filename) {
+    CHECK(day19::part1(res) == expectedPart1);
+  }
+  SECTION("Part2 : " + filename) {
+    CHECK(day19::part2(res) == expectedPart2);
+  }
+}
